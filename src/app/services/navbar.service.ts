e5fbec264573af4f429994ff1059dd849ecffa51
Roberto Ariel Latorraca <73842487+RobertoLatorraca@users.noreply.github.com>
2022-12-25 10:07:16 -0300

    Responsive navbar. feature#006_navbar

---
+++ src/app/services/navbar.service.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavbarService {
+
+  links: Observable<any> = of(
+    {
+      title: "Dashboard",
+      url: "",
+      icon: "fas fa-home"
+    },
+    {
+      title: "Accounts",
+      url: "accounts",
+      icon: "fas fa-cubes"
+    },
+    {
+      title: "Calendar",
+      url: "",
+      icon: "fas fa-calendar-alt"
+    },
+    {
+      title: "Settings",
+      url: "",
+      icon: "fas fa-cog",
+      sublinks: [
+        {
+          title: "Banks",
+          url: "settings/banks"
+        },
+        {
+          title: "Accounts",
+          url: "settings/accounts"
+        },
+        {
+          title: "Categories",
+          url: "settings/categories"
+        },
+        {
+          title: "Currencies",
+          url: "settings/currencies"
+        },
+        {
+          title: "Payees",
+          url: "settings/payees"
+        },
+        {
+          title: "Tags",
+          url: "settings/tags"
+        }
+      ]
+    },
+    {
+      title: "Accounts",
+      url: "accounts",
+      icon: "fas fa-cubes"
+    },
+    {
+      title: "Calendar",
+      url: "",
+      icon: "fas fa-calendar-alt",
+      sublinks: [
+        {
+          title: "Banks",
+          url: "settings/banks"
+        },
+        {
+          title: "Accounts",
+          url: "settings/accounts"
+        },
+        {
+          title: "Categories",
+          url: "settings/categories"
+        }
+      ]
+    }
+  );
+
+  constructor() { }
+
+  public getNavbarLinks(): Observable<any> {
+    return this.links;
+  }
+
+}
